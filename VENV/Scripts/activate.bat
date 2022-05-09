15319a3a4e884196e92fd5eb8f69aeb67bd54069
Harsh Wadhwa <80612437+harsh-wadhwa@users.noreply.github.com>
2022-05-09 18:01:47 +0530

    UI improvements

--- VENV/Scripts/activate.bat
+++ VENV/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Harsh Wadhwa\Documents\GitHub\trading-platform\VENV
+set VIRTUAL_ENV=C:\Users\Harsh Wadhwa\Desktop\SEM 6\Capstone I\PROJECT\django-ecommerce-master\VENV
 
 if not defined PROMPT set PROMPT=$P$G
 
